508de244f9727d67fe3fd106ef2fb9d35459bcdc
jbfink <john.fink@gmail.com>
2020-08-07 19:39:30 -0400

    adding dependencies and headers for graphical output

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+// Demo code for Grove - Temperature Sensor V1.1/1.2
+// Loovee @ 2015-8-26
+#include"TFT_eSPI.h"
+#include"Free_Fonts.h"
+#include <math.h>
+ 
+const int B = 4275;               // B value of the thermistor
+const int R0 = 100000;            // R0 = 100k
+const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
+ 
+#if defined(ARDUINO_ARCH_AVR)
+#define debug  Serial
+#elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
+#define debug  SerialUSB
+#else
+#define debug  Serial
+#endif
+ 
+void setup()
+{
+    Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void loop()
+{
+    int a = analogRead(pinTempSensor);
+ 
+    float R = 1023.0/a-1.0;
+    R = R0*R;
+ 
+    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+ 
+    Serial.print("temperature = ");
+    Serial.println(temperature);
+ 
+    delay(1000);
 }
