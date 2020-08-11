b137f27e534445b04aaf3210ac2342c941f9e1a3
jbfink <john.fink@gmail.com>
2020-08-11 09:28:26 -0400

    Making temperature display a function.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 
 #include"TFT_eSPI.h"
 #include <math.h>
-//#include"seeed_line_chart.h" 
 #define LCD_BACKLIGHT (72Ul) // Control Pin of LCD
 TFT_eSPI tft;
 #define max_size 50 //maximum size of data
@@ -21,14 +20,52 @@ const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
 
 
 // For non-blocking delay: https://dzone.com/articles/arduino-using-millis-instead-of-delay
-int period = 600000;
+//int period = 600000;
+int period = 1000;
 unsigned long time_now = 0;
 
+void displayTemp() {
+    // setting up thermometer
+    int a = analogRead(pinTempSensor);
+    float R = 1023.0/a-1.0;
+    R = R0*R;
+    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+// 
+    spr.fillSprite(TFT_WHITE);
+    if (data.size() == max_size) {
+        data.pop();//this is used to remove the first read variable
+    }
+    data.push(temperature); // put temperature in data stack
 
+    //Settings for the line graph title
+    auto header =  text(0, 0)
+        .value("temp in C")
+        .align(center)
+        .valign(vcenter)
+        .width(tft.width())
+        .thickness(3);
+
+    header.height(header.font_height() * 2);
+    header.draw(); //Header height is the twice the height of the font
+
+  //Settings for the line graph
+    auto content = line_chart(20, header.height()); //(x,y) where the line graph begins
+    content
+        .height(tft.height() - header.height() * 1.5) //actual height of the line chart
+        .width(tft.width() - content.x() * 2) //actual width of the line chart
+        .based_on(15.0) //Starting point of y-axis, must be a float. Set to 15C in summer. :D
+        .show_circle(true) //drawing a cirle at each point, default is on.
+        .value(data) //passing through the data to line graph
+        .color(TFT_PURPLE) //Setting the color for the line
+        .draw();
+
+    spr.pushSprite(0, 0);
+}
 
 void setup()
 {
     pinMode(WIO_5S_PRESS, INPUT_PULLUP);
+    pinMode(WIO_KEY_C, INPUT_PULLUP);
     tft.begin();
     tft.setRotation(3);
     spr.createSprite(TFT_HEIGHT, TFT_WIDTH);
@@ -39,42 +76,8 @@ void loop()
 {
     if (millis() > time_now + period) {
         time_now = millis();
+        displayTemp();
 
-        // setting up thermometer
-        int a = analogRead(pinTempSensor);
-        float R = 1023.0/a-1.0;
-        R = R0*R;
-        float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
-    // 
-        spr.fillSprite(TFT_WHITE);
-        if (data.size() == max_size) {
-            data.pop();//this is used to remove the first read variable
-        }
-        data.push(temperature); // put temperature in data stack
-
-        //Settings for the line graph title
-        auto header =  text(0, 0)
-            .value("temp in C")
-            .align(center)
-            .valign(vcenter)
-            .width(tft.width())
-            .thickness(3);
-
-        header.height(header.font_height() * 2);
-        header.draw(); //Header height is the twice the height of the font
-
-      //Settings for the line graph
-        auto content = line_chart(20, header.height()); //(x,y) where the line graph begins
-        content
-            .height(tft.height() - header.height() * 1.5) //actual height of the line chart
-            .width(tft.width() - content.x() * 2) //actual width of the line chart
-            .based_on(15.0) //Starting point of y-axis, must be a float. Set to 15C in summer. :D
-            .show_circle(true) //drawing a cirle at each point, default is on.
-            .value(data) //passing through the data to line graph
-            .color(TFT_PURPLE) //Setting the color for the line
-            .draw();
-
-        spr.pushSprite(0, 0);
     }
     int currentState = digitalRead(LCD_BACKLIGHT);
     if (digitalRead(WIO_5S_PRESS) == LOW) {
